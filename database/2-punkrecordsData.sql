2e625637e34d748ee59d134dbc99b6ffefc13957
Asier <acasiergonzalez@gmail.com>
2025-04-21 11:02:31 +0200

    archivos docker creados y datos de la DB introducidos

---
+++ database/2-punkrecordsData.sql
@@ -0,0 +1,9 @@
+USE punkrecords;
+
+INSERT INTO punkrecords.users (nickname, email, password, user_img)
+VALUES
+('CarlosPunk', 'carlos.punk@email.com', 'password123', 'img_carlos.jpg'),
+('AnaRock', 'ana.rock@email.com', 'password123', 'img_ana.jpg'),
+('PedroVibe', 'pedro.vibe@email.com', 'password123', 'img_pedro.jpg'),
+('LuciaSound', 'lucia.sound@email.com', 'password123', 'img_lucia.jpg'),
+('JavierBeats', 'javier.beats@email.com', 'password123', 'img_javier.jpg');
