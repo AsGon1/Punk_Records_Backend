5aa516aef3c817a8fa1f56de52a2bd80238abb72
Asier <acasiergonzalez@gmail.com>
2025-04-23 16:38:38 +0200

    Routers terminados

--- src/routes/api/apiRouter.js
+++ src/routes/api/apiRouter.js
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import authRouter from "./authRouter.js";
 import favoriteRouter from "./favoriteRouter.js"
+import reviewRouter from "./reviewRouter.js"
 
 const router = Router();
 
 router.get("/", (req, res) => {
-    res.send("welcome to Punk Records API");
+    res.send("Welcome to Punk Records API");
 })
 
 router.use("/", authRouter);
 router.use("/favorite", favoriteRouter);
+router.use("/review", reviewRouter);
 
 export default router
