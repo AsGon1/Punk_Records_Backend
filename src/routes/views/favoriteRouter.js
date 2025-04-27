578f5f6884dce020a2d71ea87d32c61542056679
Asier <acasiergonzalez@gmail.com>
2025-04-27 13:35:47 +0200

    View routers hechos

---
+++ src/routes/views/favoriteRouter.js
@@ -0,0 +1,27 @@
+import {Router} from "express";
+import favoriteViewController from "../../controllers/favorite/favoriteViewController.js"
+import { isLoggedInAPI } from "../../middleware/authMiddleware.js";
+
+const router = Router();
+
+// conseguir todos los favoritos del usuario
+router.get("/",isLoggedInAPI,favoriteViewController.getAllUserFavorites)
+
+// crear un favorito
+router.get("/create",favoriteViewController.createForm);
+router.post("/",favoriteViewController.create)
+
+// conseguir favorito por id
+router.get("/:id",favoriteViewController.getByID)
+
+// conseguir favorito por id de la api externa
+router.get("/media/:media_id",favoriteViewController.getByMediaID)
+
+// modificar un favorito
+router.get("/:id/edit",favoriteViewController.editForm)
+router.put("/:id",favoriteViewController.edit)
+
+// ruta para eliminar un favorito
+router.delete("/:id",favoriteViewController.remove)
+
+export default router;
